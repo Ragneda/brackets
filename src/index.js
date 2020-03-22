b732889eec2d57ca6a3d7f6ff5f5404882ab467e
Ragneda <53915581+Ragneda@users.noreply.github.com>
2020-03-22 23:42:45 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,27 @@
 module.exports = function check(str, bracketsConfig) {
+    let arr = [];
+    let top = -1;
+    for (let i = 0; i < str.length; i++) {
+        for (let j = 0; j < bracketsConfig.length; j++) {
+            if (str[i] == bracketsConfig[j][0]) {
+                arr[++top] = str[i];
+                break;
+            }
 
-    let arr = Array.from(new Set(str));
-    if (str !== arr) {
-        return true;
+            if (str[i] == bracketsConfig[j][1]) {
+                if (top == -1) {
+                    return false;
+                }
+
+                if (arr[top--] != bracketsConfig[j][0]) {
+                    return false;
+                }
+            }
+        }
     }
-    return false;
+    if (top != -1) {
+        return false;
+    }
+    return true;
 };
+
